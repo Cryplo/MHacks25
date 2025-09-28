07a53c650aae5c31b788ff29dc45e0531c81f907
Bill Hong <billhong@umich.edu>
2025-09-27 21:01:18 -0400

    Deleted tab change

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,10 +4,6 @@ import { Terminal } from './Terminal'
 import Tabs from './components/tab'
 
 function App(): React.JSX.Element {
-  const handleActiveTabChange = (tabId: string) => {
-    // You can handle active tab changes here if needed
-    console.log('Active tab changed to:', tabId)
-  }
 
   return (
     <>
